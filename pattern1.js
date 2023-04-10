b58d0833c4314b458e38867247e663bd2fd91f58
Dhana@1094 <dhananjaya1094@gmail.com>
2023-04-10 11:01:06 +0530

    Sample pattern program

---
+++ pattern1.js
@@ -0,0 +1,12 @@
+
+
+for(var row=1;row<=4;row++){
+    var output=" ";
+    //for every row,run the col
+    for(var col=1;col<=row;col++){
+       
+          console.log("* ");
+           output+=col;
+    }
+     console.log("* ");
+}
